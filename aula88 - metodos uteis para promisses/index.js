2f3f3fbd3e8be2f26b3fb6757f625a25272d0fef
gabrielpaess <gpaespinheiro@icloud.com>
2023-07-23 06:28:56 -0300

    estudos babel webpack

--- aula88 - metodos uteis para promisses/index.js
+++ aula88 - metodos uteis para promisses/index.js
@@ -1,4 +1,4 @@
-function rand(min, max) {
+function rand(min = 0, max = 3) {
     min *= 1000;
     max *= 1000;
     return Math.floor(Math.random() * (max - min) + min);
@@ -6,56 +6,58 @@ function rand(min, max) {
 
 function esperaAi(msg, tempo) {
     return new Promise((resolve, reject) => {
-        if(typeof msg !== 'string') {
-        reject('CAI NO ERRO');
-        return;
-        }
         setTimeout(() => {
-           resolve(msg.toUpperCase() + ' - Passei na promise'); 
-           return;
+            if(typeof msg !== 'string') {
+                reject('CAI NO ERRO');
+                return;
+            }
+            
+            resolve(msg.toUpperCase() + ' - Passei na promisse');
+            return;
         }, tempo);
     });
 }
-
-// Promise.all RETORNA TODAS AS PROMISES
-//Promise.race RETORNA A PROMISE RESOLVIDA PRIMEIRO
-//Promise.resolve RETORNA A PROMISE RESOLVIDA
-//Promise.reject RETORNA A PROMISE REJEITADA
-
-
-
-
-
-
-//const promises = [
-//    'Primeiro valor',
-//    esperaAi('Promise 1', 3000),
-//    esperaAi('Promise 2', 500),
-//    esperaAi('Promise 3', 1000),
-//    esperaAi(11111, 1000),
-//    'Outro valor'
-//];
 //
-//Promise.all(promises)
-//    .then(function(valor){
-//        console.log(valor);
+//esperaAi('Fase 1', rand())
+//    .then(valor => {
+//       console.log(valor);
+//        return esperaAi('Fase 2', rand());
 //    })
-//    .catch(function(erro){
-//        console.log(erro);
-//    });
+//    .then(fase => {
+//        console.log(fase);
+//        return esperaAi('Fase 3', rand());
+//    })
+//    .then(fase => {
+//        console.log(fase);
+//       return fase;
+//   })
+//    then(fase => {
+//        console.log('Terminamos na fase:', fase);
+//    })
+//    .catch(e => console.log(e));
 
-function baixaPagina() {
-    const emCache = true;
+async function executa() {
+    try {
+        const fase1 = await esperaAi('Fase 1', 1000);
+        console.log(fase1);
 
-    if(emCache) {
-        return Promise.reject('Página em cache');
-    } else {
-        return esperaAi('Baixei a página', 3000);
+        setTimeout(function() {
+            console.log('Essa promise estava pendente', fase1)
+        }, 1100);
+
+        const fase2 = await esperaAi('Fase 2', rand());
+        console.log(fase2);
+
+        const fase3 = await esperaAi('Fase 3', rand());
+        console.log(fase3);
+
+        console.log('Terminamos na fase:', fase3);
+    } catch(e) {
+        console.log(e);
     }
 }
 
-baixaPagina()
-    .then(dadosPagina => {
-        console.log(dadosPagina);
-    })
-    .catch(e => console.log('ERRO', e));
+//executa();
+
+const teste2 = esperaAi('qlq', 5000);
+console.log(teste2);
---
+++ aula92 - Babel e can i use/readme.js
@@ -0,0 +1,6 @@
+/*
+* Sites
+*
+* https://babeljs.io/
+* https://caniuse.com/
+*/
